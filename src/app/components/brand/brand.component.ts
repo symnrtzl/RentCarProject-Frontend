c3f25e551becd97d1f26d333b813edaa1bc8cc2f
symnrtzl <symnrtzl99@gmail.com>
2023-12-01 09:48:31 +0300

    brand table listed

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Brand } from '../../models/brand/brand';
+import { BrandService } from '../../services/brand.service';
 
 @Component({
   selector: 'app-brand',
@@ -8,6 +10,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './brand.component.html',
   styleUrl: './brand.component.css'
 })
-export class BrandComponent {
+export class BrandComponent implements OnInit {
+
+  brands: Brand[] = [];
+  dataLoaded=false;
+
+  constructor(private brandService:BrandService){}
+
+  ngOnInit(): void {
+    this.getBrands();
+  }
+
+  getBrands(){
+    this.brandService.getBrands().subscribe(response => {
+      this.brands=response.data;
+      this.dataLoaded=true;
+    })
+  }
 
 }
