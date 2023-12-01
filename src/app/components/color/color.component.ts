b5059d03d994a1c72574735dca8bb09330214b23
symnrtzl <symnrtzl99@gmail.com>
2023-12-01 10:05:08 +0300

    color table listed

--- src/app/components/color/color.component.ts
+++ src/app/components/color/color.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Color } from '../../models/color/color';
+import { ColorService } from '../../services/color.service';
 
 @Component({
   selector: 'app-color',
@@ -8,6 +10,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './color.component.html',
   styleUrl: './color.component.css'
 })
-export class ColorComponent {
+export class ColorComponent implements OnInit {
+
+  colors: Color[] = [];
+  dataLoaded = false;
+
+  constructor(private colorService:ColorService){}
+
+  ngOnInit(): void {
+    this.getColors();
+  }
+
+  getColors(){
+    this.colorService.getColors().subscribe( response => {
+      this.colors = response.data;
+      this.dataLoaded=true;
+    })
+  }
 
 }
